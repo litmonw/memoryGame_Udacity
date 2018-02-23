2f02d2e2f7f2dac81731c8130c22cafc7e7c77c8
litmonw <litmonw@163.com>
2018-02-24 00:13:11 +0800

    修改：js面向对象化

--- js/app.js
+++ js/app.js
@@ -1,64 +1,8 @@
-// 初始数据
-const arr = ['diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'anchor', 'leaf', 'bicycle', 'diamond', 'bomb', 'leaf', 'bomb', 'bolt', 'bicycle', 'paper-plane-o', 'cube'];
+// /**
+//  * 洗牌函数来自于 http://stackoverflow.com/a/2450976
+//  * @returns {iconsay} 随机排序之后的数组
+//  */
 
-const movesObj = document.getElementsByClassName('moves')[0];
-// 步数
-let moves = 0;
-
-// 匹配次数
-let matched = 0;
-
-// 星级数
-let stars = 3;
-
-// 游戏状态
-let start = false;
-
-// 避免点击过快
-let ban = false;
-
-let timeNum = document.getElementsByClassName('time')[0];
-let deck = document.getElementsByClassName('deck')[0];
-
-// 临时匹配容器
-let open = [];
-
-// 所有的卡牌
-const items = deck.childNodes;
-
-/**
- * @description  初始化数据
- */
-function init() {
-	// 卡片洗牌
-	let randomArr = shuffle(arr);
-
-	// 如果并非首次初始化
-	if (items.length >= 2) {
-		let i = items.length - 1;
-		while (i > 0) {
-			deck.removeChild(items[i]);
-			i--;
-		}
-	}
-
-	// 循环遍历每张卡片，创建其 HTML，将每张卡的 HTML 添加到页面
-	for (let i = 0; i < randomArr.length; i++) {
-		let card = document.createElement('li');
-		card.className = 'card';
-		let ie = document.createElement('i');
-		ie.className = 'fa fa-' + randomArr[i];
-		card.appendChild(ie);
-		deck.appendChild(card);
-	}
-
-	// 添加事件
-	monitor();
-}
-/**
- * 洗牌函数来自于 http://stackoverflow.com/a/2450976
- * @returns {array} 随机排序之后的数组
- */
 function shuffle(array) {
 	let currentIndex = array.length,
 		temporaryValue, randomIndex;
@@ -75,220 +19,297 @@ function shuffle(array) {
 }
 
 /**
- * @description 绑定卡牌点击事件
- */
-function monitor() {
-	for (let i = 0; i < items.length; i++) {
-		items[i].addEventListener('click', function() {
+ * @description 主游戏类
+ * @constructor
+ * @param {array} icons 可用的图标数组
+ * @param {object} deckElm - deck 所在元素
+ */ 
+var Game = function (icons, deckElm) {
+	this.icons = icons;
+	this.deckElm = deckElm;
+	
+	// 临时匹配容器
+	this.box = [];
 
-			// 首次点击时开始计时
-			if (!start) {
-				start = true;
-				calculateTime();
+	this.matchedNum = 0;
+
+	this.state = false;
+
+	this.time = 0;
+
+	this.stars = 3;
+
+	this.move = 0;
+}
+
+/**
+ * @description 原型方法，用于初始化游戏
+ */ 
+Game.prototype.init = function() {
+	let that = this; // 避免匿名函数 this 丢失绑定
+
+	// 游戏初始化
+	this.icons = shuffle(icons);
+	this.time = 0;
+	this.matchedNum = 0;
+	this.state = false;
+	this.moves = 0;
+
+	movesElm.innerText = 0;
+	timeElm.innerText = 0;
+
+
+	that.icons.map(function(icon) { // 遍历卡片
+		let card = new Card(icon);
+		card.elm.addEventListener('click', function () { // 为卡片元素绑定点击事件
+
+			// 修正下文异步时快速点击可能产生的 bug
+			if (that.box.length == 2) {
+				return false;
 			}
 
-			// 放置点击过快
-			if (ban) {
-				return false;
+			// 每点击一次卡牌增加一次移动次数
+			that.addMoves();
+
+			if (!game.state) {
+				game.state = !game.state;
+				game.calculateTime();
 			}
 
 			// 已匹配过的牌
 			if (this.classList[1] == 'match') {
+				console.log('已经匹配过的牌')
 				return false;
 			}
 
 			// 重复点击同一张卡牌
 			if (this.classList[1] == 'open') {
+				console.log('重复点击同一张牌');
 				return false;
 			}
 
-			// 增加移动次数
-			addMoves();
+			card.open(); // 翻开卡片
 
-			// 显示卡片的符号
-			showCard(this);
-
-			// 将卡片添加到状态为 'open' 的数组中
-			pushOpen(this);
-
-			// 检测两张卡是否匹配
-			if (open.length == 2) {
-				ban = true;
-				if (open[0] == open[1]) {
-					matchSuccess();
-				} else {
-					matchFail();
-				}
+			// 将卡片属性
+			that.box.push(card.elm);
+			
+			// 如果翻牌数 < 2 则不进入匹配
+			if (that.box.length < 2) {
+				return false;
 			}
 
-			if (matched == 8) {
-				gameSuccess();
+			// 匹配
+			let isMatched = that.match(); // 翻开的两张卡片是否匹配
+			// let box = that.box;
+			if (isMatched) {
+				// 如果匹配
+				console.log('匹配成功');
+				card.show(that.box);
+				game.matchedNum++;
+				that.box = [];
+			} else {
+				// 如果不匹配
+				console.log('匹配不成功');
+				setTimeout(function () {
+					card.close(that.box);
+					that.box = [];
+				}, 1000);
+				
 			}
 
+			// 游戏胜利条件
+			if (game.matchedNum == 2) {
+				that.gameSuccess();
+			}
 		});
-	}
-}
+
+		// 添加 DOM
+		that.deckElm.appendChild(card.elm);
+	});
+};
 
 /**
- * @description 移动次数更改
- */
-function addMoves() {
-	moves++;
-	movesObj.innerText = moves;
-}
-
-/**
- * @description 显示卡牌
- * @param event - Event 对象
- */
-function showCard(event) {
-	event.classList.add('open', 'show');
-}
-
-/**
- * @description 添加卡牌
- * @param event - Event 对象
- */
-function pushOpen(event) {
-	open.push(event.childNodes[0].classList[1].slice(3));
-}
-
-/**
- * 匹配卡牌成功
- */
-function matchSuccess() {
-	setTimeout(function() {
-		for (let i = 1; i < items.length; i++) {
-			if (items[i].childNodes[0].classList[1].slice(3) == open[0] || items[i].childNodes[0].classList[1].slice(3) == open[1]) {
-				items[i].classList.remove('open', 'show');
-				items[i].classList.add('match');
-			}
-		}
-
-		open = [];
-		ban = false;
-	}, 1000);
-
-	matched++;
-}
-
-/**
- * 匹配卡牌失败
- */
-function matchFail() {
-	setTimeout(function() {
-		for (let i = 1; i < items.length; i++) {
-			if (items[i].childNodes[0].classList[1].slice(3) == open[0] || items[i].childNodes[0].classList[1].slice(3) == open[1]) {
-				items[i].classList.remove('open', 'show');
-			}
-		}
-
-		// 清空临时匹配容器
-		open = [];
-		// 允许继续点击
-		ban = false;
-	}, 1000);
-}
-
-/**
- * 游戏胜利
- */
-function gameSuccess() {
-	start = false;
+ * @description  原型方法，用于游戏胜利时的游戏结算显示
+ */ 
+Game.prototype.gameSuccess = function () {
+	// 游戏状态为停止
+	game.state = false;
 
 	// 显示游戏面板
-	deck.classList.add('hide');
-	scorePanel.classList.add('hide');
-	successBoard.classList.remove('hide');
+	this.deckElm.classList.add('hide');
+	scoreElm.classList.add('hide');
+	successPanel.classList.remove('hide');
 
 	// 显示游戏最终得分
 	let movesResult = document.getElementsByClassName('moves')[1];
-	movesResult.innerText = moves;
+	movesResult.innerText = this.moves;
 	let timeResult = document.getElementsByClassName('time')[1];
-	timeResult.innerText = timeNum.innerText;
+	timeResult.innerText = this.time;
 }
 
 
 /**
- * 游戏计时
- */
-function calculateTime() {
-	let timer = setInterval(function() {
-		// 结束计时
-		if (!start) {
+ * @description  原型方法，时间计算
+ */ 
+Game.prototype.calculateTime = function () {
+	let that = this;
+	let timer = setInterval(function () {
+		if (!that.state) {
 			clearInterval(timer);
 			return false;
 		}
-
-		timeNum.innerText = parseInt(timeNum.innerText) + 1;
+		that.time++;
+		timeElm.innerText = that.time;
 
 		// 星级评定
-		calculateStars();
+		that.calculateStars();
 
 	}, 1000);
-}
 
-// 游戏首次加载
-init();
+};
 
-let repeat = document.getElementsByClassName('fa-repeat')[0];
-let scorePanel = document.getElementsByClassName('score-panel')[0];
-let successBoard = document.getElementsByClassName('success-board')[0];
-
-// 为重置绑定点击事件
-repeat.addEventListener('click', function() {
-	repeatGame();
-});
-
-let again = document.getElementsByClassName('again')[0];
-
-// 为重玩绑定点击事件
-again.addEventListener('click', function() {
-	repeatGame();
-	deck.classList.remove('hide');
-	scorePanel.classList.remove('hide');
-	successBoard.classList.add('hide');
-});
-
-function repeatGame() {
-	// 初始化所有游戏数据
-	open = [];
-	start = false;
-	timeNum.innerText = 0;
-	movesObj.innerText = 0;
-	moves = 0;
-	matched = 0;
-
-	//重置星级评分
-	stars = 3;
-	calculateStars();
-
-	// 移除旧游戏卡牌
-	for (let i = 1; i < items.length; i++) {
-		items[i].classList.remove('open', 'show', 'match');
+/**
+ * @description  原型方法，星级判定
+ */ 
+Game.prototype.calculateStars = function() {
+	let starsElm = document.getElementsByClassName('stars');
+	if (this.moves >= 30 && this.moves < 60) {
+		this.stars = 2;
+	} else if (this.moves >= 60) {
+		this.stars = 1;
 	}
+	starsElm[0].innerHTML = '';
+	starsElm[1].innerHTML = '';
+	for (let i = 0; i < this.stars; i++) {
+		for (let j = 0; j < starsElm.length; j++) {
+			starsElm[j].innerHTML += '<li><i class="fa fa-star"></i></li> ';
+		}
+	}
+};
 
-	// 重新初始化
-	init();
+/**
+ * @description  步数计算
+ */  
+Game.prototype.addMoves = function () {
+	this.moves++;
+	movesElm.innerText = this.moves;
+
 }
 
 /**
- * @description 星级评定
- */
-function calculateStars() {
-	let starsObj = document.getElementsByClassName('stars');
-	if (moves >= 30 && moves < 60) {
-		stars = 2;
-	} else if (moves >= 60) {
-		stars = 1;
+ * @description  验证匹配
+ * @return {bool} 返回匹配的正确与否
+ */ 
+Game.prototype.match = function() {
+	if (this.box[0].childNodes[0].classList[1] == this.box[1].childNodes[0].classList[1]) {
+		return true;
 	}
-	starsObj[0].innerHTML = '';
-	starsObj[1].innerHTML = '';
-	for (let i = 0; i < stars; i++) {
-		for (let j = 0; j < starsObj.length; j++) {
-			starsObj[j].innerHTML += '<li><i class="fa fa-star"></i></li> ';
+
+	return false;
+};
+
+/**
+ * @description 原型方法，用于重新开始游戏前清除卡片网页元素
+ */
+Game.prototype.repeat = function () {
+	let items = deckElm.childNodes;
+	// 移除旧游戏卡牌
+	for (let i = 1; i < items.length; i++) {
+		while (items.length > 1) {
+			deckElm.removeChild(items[items.length - 1]);
 		}
 	}
 
+	game.init();
+};
 
+
+let Card = function (icon) {
+	this.icon = icon;
+	this.elm = this.generateElm();
+
+	// 更多实例变量
 }
+
+/**
+ * @description 原型方法，用于生成卡片对应的网页元素
+ * @returns {object} 卡片网页元素
+ */
+Card.prototype.generateElm = function() {
+    var liElm = document.createElement('li');
+    liElm.className = 'card';
+    var iElm = document.createElement('i');
+    iElm.className = `fa fa-${this.icon}`;
+    liElm.appendChild(iElm);
+    return liElm;
+}
+
+/**
+ * @description 为卡片元素设置翻开卡片的 CSS 类
+ */
+Card.prototype.open = function() {
+    this.elm.className = 'card open show';
+}
+
+/**
+ * @description 为卡片元素设置匹配卡片的 CSS 类
+ */
+Card.prototype.show = function(box) {
+	box[0].className = 'card match';
+	box[1].className = 'card match';
+};
+
+/**
+ * @description 为卡片元素设置关闭卡片的 CSS 类
+ */
+Card.prototype.close = function(box) {
+	box[0].className = 'card';
+	box[1].className = 'card';
+}
+
+
+// 初始数据
+// 卡片数组
+const icons = ['diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'anchor', 'leaf', 'bicycle', 'diamond', 'bomb', 'leaf', 'bomb', 'bolt', 'bicycle', 'paper-plane-o', 'cube'];
+
+
+// 卡片父级元素
+let deckElm = document.getElementsByClassName('deck')[0];
+
+// 事件元素
+let timeElm = document.getElementsByClassName('time')[0];
+
+// 重新开始游戏元素
+let repeatElm = document.getElementsByClassName('fa-repeat')[0];
+
+// 重新开始游戏 点击事件
+repeatElm.addEventListener('click', function () {
+	game.repeat();
+});
+
+// 移动次数元素
+let movesElm = document.getElementsByClassName('moves')[0];
+
+// 得分面板
+let scoreElm = document.getElementsByClassName('score-panel')[0];
+
+// 游戏胜利面板
+let successPanel = document.getElementsByClassName('success-panel')[0];
+
+let againElm = document.getElementsByClassName('again')[0];
+
+// 为重玩绑定点击事件
+againElm.addEventListener('click', function() {
+
+	deckElm.classList.remove('hide');
+	scoreElm.classList.remove('hide');
+	successPanel.classList.add('hide');
+
+	game.repeat();
+});
+
+// 创建游戏实例
+let game = new Game(icons, deckElm);
+
+// 首次初始化游戏
+game.init();
